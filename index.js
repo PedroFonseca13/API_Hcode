ac41edcc5567f95fcc741ef7a3389b1975f4b9fb
Pedro Fonseca <pedrinhopedro01@gmail.com>
2023-01-24 09:27:27 -0300

    feat: implementa o express-validator no codigo

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const consign = require('consign')
+const expressValidator = require('express-validator')
+
 const app = express()
 
 app.use(express.json());
+app.use(expressValidator())
 consign().include('routes').include('utils').into(app)
 
 const port = 3000
